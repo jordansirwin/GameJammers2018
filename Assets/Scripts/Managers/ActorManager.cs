2ba7c8d10e0cc3eff73dcb6f32622e4c966d1c78
jirwin <jordan.s.irwin@gmail.com>
2018-05-04 21:12:59 -0700

    MapManager despawns objects off screen

--- Assets/Scripts/Managers/ActorManager.cs
+++ Assets/Scripts/Managers/ActorManager.cs
@@ -4,15 +4,29 @@ using System.Collections;
 public class ActorManager : MonoBehaviour
 {
     private float _speed;
+    private Vector3 _despawnPosition;
 
     public void SetSpeed(float speed) {
         _speed = speed;
     }
 
+    public void SetDespawnBoundary(Vector3 position) {
+        _despawnPosition = position;
+    }
+
 	// Update is called once per frame
 	void Update()
 	{
+        if(transform.position.y >= _despawnPosition.y) {
+            Despawn();
+            return;
+        }
+
         // move the actor "up" the screen by their speed
         transform.Translate(Vector3.up * _speed * Time.deltaTime);
 	}
+
+    private void Despawn() {
+        Destroy(this.gameObject);
+    }
 }
