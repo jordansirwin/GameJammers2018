19e5e777576ea0c1b5abeecd93b78d5ec209450e
Taylor <tclustuff@gmail.com>
2018-05-06 20:05:01 -0700

    Avalanche size prototyping

---
+++ Assets/Scripts/AvalancheVisual.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvalancheVisual : MonoBehaviour
+{
+    [SerializeField] private AvalancheLayer[] _layers;
+    [SerializeField] private float _size = 0;
+
+    private void Start()
+    {
+        foreach (var layer in _layers)
+        {
+            layer.Particles.Stop();
+            SetEmissionRate(layer.Particles, 0);
+        }
+    }
+
+    private void Update()
+    {
+        foreach (var layer in _layers)
+        {
+            if(_size >= layer.StartAtSize)
+            {
+                if (!layer.Particles.isPlaying)
+                {
+                    Debug.Log("Start playing " + layer.Particles);
+                    layer.Particles.Play();
+                }
+
+                if (layer.Particles.emission.rateOverTime.constant < layer.MaxEmission)
+                {
+                    //float step = (layer.MaxAtSize - _size) / (layer.StartAtSize - layer.MaxAtSize);
+                    if (_size >= layer.MaxAtSize)
+                    {
+                        SetEmissionRate(layer.Particles, layer.MaxEmission);
+                    }
+                    else
+                    {
+                        float step = (_size - layer.StartAtSize) / (layer.MaxAtSize - layer.StartAtSize);
+                        SetEmissionRate(layer.Particles, Mathf.Lerp(0, layer.MaxEmission, step));
+                    }
+                }
+            }
+        }
+    }
+
+    private void SetEmissionRate(ParticleSystem system, float rateAmount)
+    {
+        var emission = system.emission;
+        var rate = emission.rateOverTime;
+        rate.constant = rateAmount;
+        emission.rateOverTime = rate;
+    }
+
+    [Serializable]
+    public class AvalancheLayer
+    {
+        [SerializeField] public ParticleSystem Particles;
+        [SerializeField] public float StartAtSize;
+        [SerializeField] public float MaxAtSize;
+        [SerializeField] public float MaxEmission;
+    }
+}
